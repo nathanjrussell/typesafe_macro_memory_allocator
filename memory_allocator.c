40c905e9cdd9d7211b6ea000fd1c4a0b111947cb
Nathan Russell <nathan@blackpathway.com>
2024-03-28 20:19:25 -0400

    Updated code to match required function signatures

--- memory_allocator.c
+++ memory_allocator.c
@@ -3,7 +3,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-void* allocate_memory(size_t size) {
+void* mymalloc(size_t size) {
     void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (ptr == MAP_FAILED) {
         perror("Memory allocation failed");
@@ -12,17 +12,15 @@ void* allocate_memory(size_t size) {
     return ptr;
 }
 
-int deallocate_memory(void* ptr, size_t size) {
+void myfree(void* ptr, size_t size) {
     if (munmap(ptr, size) == -1) {
         perror("Memory deallocation failed");
-        return 1;
     }
-    return 0;
 }
 
 int main() {
     // Allocate memory for an integer
-    int *ptr = (int*) allocate_memory(sizeof(int));
+    int *ptr = (int*) mymalloc(sizeof(int));
     if (ptr == NULL) {
         return 1;
     }
@@ -32,8 +30,7 @@ int main() {
     printf("Value of *ptr: %d\n", *ptr);
 
     // Deallocate the memory
-    if (deallocate_memory(ptr, sizeof(int)) != 0) {
-        return 1;
-    }
+    myfree(ptr, sizeof(int));
 
-    
+    return 0;
+}
