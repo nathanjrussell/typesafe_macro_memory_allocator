8551e0478a5c4704a97ab802b156300e7eeec7af
Nathan Russell <nathan@blackpathway.com>
2024-03-28 20:44:19 -0400

    Pulled initial memory allocation to new function

--- memory_allocator.c
+++ memory_allocator.c
@@ -5,36 +5,47 @@
 
 #define MEMORY_SIZE 1024 * 1024 // 1 MB
 
+typedef struct {
+    size_t size;
+    int free;
+} Block;
+
 static char *memory = NULL;
 static size_t allocated = 0;
 
-void* mymalloc(size_t size) {
+void init_memory() {
     if (memory == NULL) {
         // Reserve a large block of memory
         memory = mmap(NULL, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         if (memory == MAP_FAILED) {
             perror("Memory reservation failed");
-            return NULL;
+            exit(EXIT_FAILURE);
         }
     }
+}
 
-    if (allocated + size > MEMORY_SIZE) {
+void* mymalloc(size_t size) {
+    init_memory();
+
+    if (allocated + size + sizeof(Block) > MEMORY_SIZE) {
         // Not enough memory left
         return NULL;
     }
 
     // Allocate a chunk from the reserved block
-    void *ptr = memory + allocated;
-    allocated += size;
+    Block *block = (Block *)(memory + allocated);
+    block->size = size;
+    block->free = 0;
 
-    return ptr;
+    allocated += size + sizeof(Block);
+
+    return (void *)(block + 1);
 }
 
-void myfree(void* ptr, size_t size) {
-    // This function doesn't actually free the memory, it just moves the allocation pointer back
-    if (ptr >= memory && ptr < memory + allocated) {
-        allocated -= size;
-    }
+void myfree(void* ptr) {
+    // Mark the block as free
+    Block *block = (Block *)ptr - 1;
+    block->free = 1;
 }
 
 int main() {
@@ -48,8 +59,8 @@ int main() {
     *(int*)ptr = 10;
     printf("Value of *ptr: %d\n", *(int*)ptr);
 
-    // "Free" the memory
-    myfree(ptr, sizeof(int));
+    // Free the memory
+    myfree(ptr);
 
     return 0;
 }
