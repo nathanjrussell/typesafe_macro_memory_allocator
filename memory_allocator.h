01ae39d12f127d458d16c247dd128a77305687dd
Nathan J. Russell <nathan@blackpathway.com>
2024-03-29 18:58:49 -0400

    Added comments to header file for memory allocator

--- memory_allocator.h
+++ memory_allocator.h
@@ -3,14 +3,15 @@
 
 #include <stdlib.h>
 
+// Block structure to keep track of the size of the block and if it is free
 typedef struct {
     size_t size;
     char free;
 } Block;
 
-void initializeMemory(size_t size);
-void free_memory();
-void* mymalloc(size_t size);
-void myfree(void* ptr);
+void initializeMemory(size_t size); // Initialize the memory allocator
+void free_memory(); // Free the memory allocated by the memory allocator
+void* mymalloc(size_t size); // Allocate memory from the memory pool
+void myfree(void* mem); // Free memory from the memory pool
 
 #endif // MEMORY_ALLOCATOR_H
